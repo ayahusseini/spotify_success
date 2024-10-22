806f840c97ba75cf7e3707be2518b6dbe33ec322
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-22 23:36:27 +0100

    added imports

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,5 +1,10 @@
 """A script to extract data from the kaggle API"""
+import os
+
 import pandas as pd
+from dotenv import load_dotenv
+import opendatasets as od
+
 
 if __name__ == "__main__":
     pd.DataFrame([[]])
