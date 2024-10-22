573abc78fbb8a2a4d94d75f0aca1d06ca591f330
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-23 00:13:30 +0100

    extraction complete

---
+++ pipeline/README.md
@@ -0,0 +1,19 @@
+# Pipeline
+
+## Setup 
+
+1. Setup virtual environment
+```sh
+python3 -m venv venv
+```
+
+2. Download requirements 
+```sh
+pip install -r requirements.txt
+```
+
+3. Setup environment variables 
+```sh
+KAGGLE_USERNAME=XXXXXXX
+KAGGLE_KEY=XXXXXXXXXXXXXXXX
+```
--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,11 +1,21 @@
 """A script to extract data from the kaggle API"""
-import os
+
+from os import environ as ENV, rename
 
 import pandas as pd
 from dotenv import load_dotenv
-import opendatasets as od
+import kagglehub
+
+SPOTIFY_DATASET = "maharshipandya/-spotify-tracks-dataset"
+DOWNLOAD_PATH = "data"
+
+
+def download_csv(dataset_name: str, download_path: str) -> None:
+    """Returns an authenticated api"""
+    load_dotenv()
+    path = kagglehub.dataset_download(dataset_name)
+    rename(path, download_path)
 
 
 if __name__ == "__main__":
-    pd.DataFrame([[]])
-    raise NotImplementedError
+    download_csv(SPOTIFY_DATASET, DOWNLOAD_PATH)
