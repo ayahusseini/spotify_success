64764ccc7041df180ce657be42ac3524a49255f0
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-23 14:49:14 +0100

    added functions for cleaning data

---
+++ pipeline/data_cleaning.py
@@ -0,0 +1,18 @@
+"""A script to clean the extracted data"""
+import pandas as pd
+
+
+def clean_duplicate_track_ids(spotify_data: pd.DataFrame):
+    """Drops duplicate rows from the dataframe"""
+
+
+def clean_bounded_column(column: pd.Series, min: float, max: float | None) -> pd.Series:
+    """Removes entries outside of the minimum and maximum bounds"""
+
+
+def feature_selection(spotify_data: pd.DataFrame) -> pd.DataFrame:
+    """Return the dataframe with only the relevant features"""
+
+
+def clean_data(spotify_data: pd.DataFrame) -> pd.DataFrame:
+    """Cleans the dataframe"""
