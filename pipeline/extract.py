5d8a7b809f64672ed6656dead21a708c65808854
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-23 10:05:48 +0100

    finalised extraction

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,21 +1,39 @@
 """A script to extract data from the kaggle API"""
 
-from os import environ as ENV, rename
-
+from os import rename, remove
+import os
 import pandas as pd
 from dotenv import load_dotenv
 import kagglehub
 
 SPOTIFY_DATASET = "maharshipandya/-spotify-tracks-dataset"
-DOWNLOAD_PATH = "data"
+DOWNLOAD_PATH = "dataset.csv"
 
 
 def download_csv(dataset_name: str, download_path: str) -> None:
-    """Returns an authenticated api"""
+    """Downloads the dataset as a CSV file"""
     load_dotenv()
-    path = kagglehub.dataset_download(dataset_name)
+    path = kagglehub.dataset_download(
+        dataset_name, force_download=True)
+    path += f"/{os.listdir(path)[0]}"
     rename(path, download_path)
 
 
+def load_csv_as_df(filepath: str) -> pd.DataFrame:
+    """Loads a CSV file as a dataframe"""
+    return pd.read_csv(filepath)
+
+
+def extract(dataset: str, download_filepath: str, delete_csv: bool) -> pd.DataFrame:
+    """Extracts the data as a dataframe.
+    Optionally, deletes the CSV downloaded"""
+    download_csv(dataset, download_filepath)
+    data = load_csv_as_df(download_filepath)
+    if delete_csv:
+        remove(download_filepath)
+    return data
+
+
 if __name__ == "__main__":
-    download_csv(SPOTIFY_DATASET, DOWNLOAD_PATH)
+    data = extract(SPOTIFY_DATASET, DOWNLOAD_PATH, True)
+    print(data.columns)
