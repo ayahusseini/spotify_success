8b3920fa1f9313f45c8f78387fbd6ad99985470e
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-27 19:38:26 +0000

    renamed function

--- pipeline/split_test_train.py
+++ pipeline/split_test_train.py
@@ -7,7 +7,7 @@ def is_in_test(track_id:str, test_ratio:float = 0.2) -> bool:
     hash_val = int(hashlib.sha256(track_id.encode()).hexdigest(),16)
     return hash_val  < test_ratio * 2**256
 
-def test_train_split(data:pd.DataFrame) -> tuple:
+def perform_test_train_split(data:pd.DataFrame) -> tuple:
     """Returns the test set and the train set"""
     is_test = data["track_id"].apply(is_in_test)
     return data[is_test], data[~is_test]
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     sample_data = pd.DataFrame({
     'track_id': ['abc123xyz789', 'def456uvw012', 'ghi789rst345', 'jkl012opq678','6rqhFgbbKwnb9MLmUQDhG6']
     })
-    test, train = test_train_split(sample_data)
+    test, train = perform_test_train_split(sample_data)
     print(f"original length = {len(sample_data)}")
     print(f"test set length: {len(test)}")
     print(f"train set length: {len(train)}")
--- pipeline/test/test_split_test_train.py
+++ pipeline/test/test_split_test_train.py
@@ -1,3 +1,12 @@
 """Tests the split_test_train.py file"""
 import pandas as pd 
-from spli
+from split_test_train import perform_test_train_split
+
+
+def test_test_train_split_consistency(sample_data):
+
+    test_set_1, train_set_1 = perform_test_train_split(sample_data)
+    test_set_2, train_set_2 = perform_test_train_split(sample_data)
+
+    assert set(test_set_1["track_id"]) == set(test_set_2["track_id"]), "Inconsistent test set across runs"
+    assert set(train_set_1["track_id"]) == set(train_set_2["track_id"]), "Inconsistent train set across runs"
