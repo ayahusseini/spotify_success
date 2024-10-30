a417ddc659a9039b6e7455123e9d846806f5b39f
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-30 09:02:59 +0000

    created basic functions

--- transform.py
+++ transform.py
@@ -1,1 +1,16 @@
 """Script for transforming the extracted data."""
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def create_numerical_pipeline() -> Pipeline:
+    """Creates and returns a pipeline for the numerical attributes"""
+
+
+def create_full_pipeline() -> Pipeline:
+    """Creates and returns a pipeline for cleaning the invalid values, scaling the numerical attributes, and encoding the categorical attributes."""
+
+
+def transform_data(data):
+    """Transforms the extracted data."""
