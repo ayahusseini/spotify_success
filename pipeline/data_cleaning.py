a9a7ac166fdd5a3d99e66d1289ac70f429cbe896
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-30 08:44:57 +0000

    removed logging statements

--- pipeline/data_cleaning.py
+++ pipeline/data_cleaning.py
@@ -22,13 +22,10 @@ class InvalidDataCleaner(BaseEstimator, TransformerMixin):
     def transform(self, X: pd.DataFrame, y=None) -> pd.DataFrame:
         """Transforms the data"""
         X = X.dropna()
-        print(f"After dropna: {X.shape[0]} rows")
 
         X = self._feature_selection(X)
-        print(f"After feature selection: {X.shape[0]} rows")
 
         X = self._clean_duplicate_track_ids(X)
-        print(f"After removing duplicates: {X.shape[0]} rows")
 
         X = self._clean_bounded_column(
             X, 'popularity', self.popularity_lower, self.popularity_upper)
@@ -40,13 +37,10 @@ class InvalidDataCleaner(BaseEstimator, TransformerMixin):
             X = self._clean_bounded_column(X, col, lower=0, upper=1)
 
         X = X[X['key'].isin(range(-1, 12))]
-        print(f"After filtering key: {X.shape[0]} rows")
 
         X = X[X['time_signature'].isin(range(3, 8))]
-        print(f"After filtering time_signature: {X.shape[0]} rows")
 
         X = X[X['mode'].isin([0, 1])]
-        print(f"After filtering mode: {X.shape[0]} rows")
 
         X['track_name_sentiment'] = self._get_track_name_sentiment(
             X['track_name'])
@@ -54,6 +48,7 @@ class InvalidDataCleaner(BaseEstimator, TransformerMixin):
         return X
 
     def _clean_bounded_column(self, data, col_name, lower=None, upper=None):
+        """Cleans out-of-bounds data"""
         if lower is not None:
             data = data[data[col_name] >= lower]
         if upper is not None:
@@ -61,10 +56,16 @@ class InvalidDataCleaner(BaseEstimator, TransformerMixin):
         return data
 
     def _clean_duplicate_track_ids(self, data):
+        """Removes duplicate track ids"""
         return data.drop_duplicates()
 
     def _feature_selection(self, data):
+        """Drops irrelevant features"""
         return data.drop(columns=['album_name', 'track_genre', 'artists'], errors='ignore')
+
+
+class TrackSentimentAdder(BaseEstimator, TransformerMixin):
+    """Custom transformer to add a track name sentiment attribute"""
 
     def _get_track_name_sentiment(self, track_name):
         return track_name.apply(lambda x: self.sia.polarity_scores(x)['compound'])
